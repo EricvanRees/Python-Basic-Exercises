38f0e1d8d12d20be87581009a19a78291f57e166
Eric van Rees <ericvanrees@gmail.com>
2025-04-24 10:43:29 +0200

    add Python script

--- 01 Basic Exercises/03 Mastering Python/08 Exercise 8.py
+++ 01 Basic Exercises/03 Mastering Python/08 Exercise 8.py
@@ -2,19 +2,23 @@
 Create a 3x3 list of lists with random values and normalize it.
 """
 
-alist = [[2,4,10,6,8,4],[2,4,10,6,8,4],[2,4,10,6,8,4]]
+import random
 
-def normalize(alist):
-  list_of_lists = []
- 
-  for list in alist:
+def normalize_lists():  
+  # create 3 lists of 3 random integers each   
+  list_of_lists = [[random.randint(1,100) for i in range(3)] for j in range(3)]
+  # create empty new lists for 3 normalized lists
+  new_list_of_lists = []   
+  # iterate over each list and normalize each element
+  for list in list_of_lists:
     newlist = []
     for i in list:
       i_normalized = (i - min(list)) / (max(list) - min(list))
+      # append normalized element to new list
       newlist.append(i_normalized)
-    
-    list_of_lists.append(newlist) 
-  return list_of_lists
+    # append normalized list elements to list of lists  
+    new_list_of_lists.append(newlist)
+  print(new_list_of_lists)
 
-print(normalize(alist))
+normalize_lists()
     
