f982200b5801d7cf72a99c6cd46b519a15f7723e
Eric van Rees <ericvanrees@gmail.com>
2025-05-01 16:06:35 +0200

    add Python script

--- 09 File Handling/01 File I O/03 Exercise 3.py
+++ 09 File Handling/01 File I O/03 Exercise 3.py
@@ -2,3 +2,13 @@
 Write a Python program to append text to a file and display the text.
 """
 
+print("***Append info to file***")
+
+file_name = r"C:\\Users\\mipc\\Downloads\\test.txt"
+
+with open(file_name, 'a') as file:
+  # append info
+  file.write("\nAppending info...\n")
+  file.write("Exiting appending info...\n")
+
+print(f'Appended info to file {file_name}')
