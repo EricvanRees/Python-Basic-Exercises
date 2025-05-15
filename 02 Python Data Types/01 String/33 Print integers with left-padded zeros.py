0cc8e9d90aac117c8ada7026159e2a9a13222600
Eric van Rees <ericvanrees@gmail.com>
2025-05-15 09:38:33 +0200

    add Python scripts

---
+++ 02 Python Data Types/01 String/33 Print integers with left-padded zeros.py
@@ -0,0 +1,6 @@
+"""
+Write a Python program to print the following integers with zeros to the left of the specified width.
+"""
+
+mynum = 2
+print(f"{mynum:{0}>5}")
---
+++ 02 Python Data Types/01 String/34 Print integers with star right-padded.py
@@ -0,0 +1,6 @@
+"""
+Write a Python program to print the following integers with '*' to the right of the specified width.
+"""
+
+mynum = 2
+print(f"{mynum:{"*"}<10}")
