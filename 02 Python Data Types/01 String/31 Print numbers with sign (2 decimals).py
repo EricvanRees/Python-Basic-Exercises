353ecaa1044aa5bf01d61e0e77f027e7943b0234
Eric van Rees <ericvanrees@gmail.com>
2025-05-15 09:22:28 +0200

    add Python scripts

---
+++ 02 Python Data Types/01 String/31 Print numbers with sign (2 decimals).py
@@ -0,0 +1,6 @@
+"""
+Write a Python program to print the following numbers up to 2 decimal places with a sign.
+"""
+
+mynumber = 2.34567
+print(f"mynumber is +{mynumber:.2f}")
---
+++ 02 Python Data Types/01 String/32 Print numbers without decimal places.py
@@ -0,0 +1,7 @@
+"""
+Write a Python program to print the following positive and negative numbers with no decimal places.
+"""
+
+mynumber = -12.999
+
+print(f"mynumber with no decimal places is {round(mynumber)}")
